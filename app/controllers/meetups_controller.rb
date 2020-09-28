f8bf24ef63627c992e3c75fb1f4261d3890a8590
SDBoahen <SDBoahen@gmail.com>
2020-09-28 16:21:50 -0400

    meetup done

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -11,24 +11,34 @@ class MeetupsController < ApplicationController
         @meetup = Meetup.new 
     end 
     def create
-        meetup = Meetup.create
+        @meetup = Meetup.create(meetup_params)
+                redirect_to meetups_path
 
-        if meetup.vaid?
-           redirect_to meetup_path(meetup)
-        else
-            #flash
-            redirect_to new_meetup_path
-        end
+
+        # if @meetup.valid?
+        #    redirect_to meetup_path(meetups)
+        # else
+        #     flash[:user_errors] = @meetup.errors.full_messages
+        #     redirect_to new_meetup_path
+        # end
 
     end
     
     def edit
+        @meetup = Meetup.find(params[:id])
     end 
     def update 
     end
 
     def delete
         
+    end 
+    
+    
+    private
+    
+    def meetup_params
+        params.require(:meetup).permit(:purpose, :time, :user_id, :location_id )
     end 
 
 end
--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,4 +2,9 @@ class Meetup < ApplicationRecord
     belongs_to :user
     belongs_to :location
     
+
+    validates :time, presence: true
+    validates :purpose, presence: true 
+
+    
 end
