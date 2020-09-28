215d7d43e1a2ade292c336ca4030bab1575999a8
SDBoahen <SDBoahen@gmail.com>
2020-09-28 14:51:12 -0400

    Establishing Flow -SDB

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,14 +1,25 @@
 class MeetupsController < ApplicationController
 
     def index
-        swdefgbhnjmk,l.k,mhgfdsza
+        @meetups = Meetup.all
     end 
     def show
+        @meetup = Meetup.find(params[:id])
     end
 
     def new
+        @meetup = Meetup.new 
     end 
     def create
+        meetup = Meetup.create
+
+        if meetup.vaid?
+           redirect_to meetup_path(meetup)
+        else
+            #flash
+            redirect_to new_meetup_path
+        end
+
     end
     
     def edit
@@ -17,6 +28,7 @@ class MeetupsController < ApplicationController
     end
 
     def delete
+        
     end 
 
 end
